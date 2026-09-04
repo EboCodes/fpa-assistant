a1d647faf751fe60bee346e003f1dbb46c7621ca
Ebofin Ayobami <ebocodes675@gmail.com>
2026-09-04 22:40:19 +0100

    style: load frontend responsive refinements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode, createElement } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import './polish.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   createElement(StrictMode, null, createElement(App)),
